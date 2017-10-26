1b1ba7364213f47889b02943e07b2e04b6a3f4fe
yukai <907022926@qq.com>
2017-10-26 13:58:48 +0800

    优化了getview 大致了解convertView的机理

--- app/src/main/java/com/example/yukai/mydefinedview/BasicView/Grid/MyGridViewAdapter.java
+++ app/src/main/java/com/example/yukai/mydefinedview/BasicView/Grid/MyGridViewAdapter.java
@@ -44,17 +44,33 @@ public class MyGridViewAdapter extends BaseAdapter{
         return position;
     }
 
+
+    //看了下guolin的博客，发现Listview、GridView也好，在getView中的convertview，
+    //是从recycleBin（滑出Listview、Gridview的view会被add进recyclerBin）中拿的view
+    //convertView可能会与每个item拥有一样的view类型，也就是不需要再inflate一遍
+    //但是这个itemView中的数据需要重新刷新
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View view = null;
+        ViewHolder viewHolder;
         if (convertView == null){
             view = mLayoutInflater.inflate(R.layout.grid_view_item_view, parent, false);
-            TextView textView = (TextView) view.findViewById(R.id.grid_view_tv);
-            textView.setText(mDatas.get(position));
+            viewHolder = new ViewHolder(view);
+            view.setTag(viewHolder);
         }else{
             view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
         }
-
+        viewHolder.mTextView.setText(mDatas.get(position));
         return view;
+    }
+
+    private class ViewHolder{
+
+        TextView mTextView;
+
+        ViewHolder(View view){
+            mTextView = (TextView) view.findViewById(R.id.grid_view_tv);
+        }
     }
 }
