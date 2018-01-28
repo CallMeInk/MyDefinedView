2dca3d407381becdbd35567edf45710649c86b15
yu_k <yu_k@Ctrip.com>
2018-01-28 13:02:42 +0800

    notification test

---
+++ app/src/main/java/com/example/yukai/mydefinedview/ArtLearningNote/chapter5_1/Chapter5_1.java
@@ -0,0 +1,137 @@
+package com.example.yukai.mydefinedview.ArtLearningNote.chapter5_1;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
+import android.widget.RemoteViews;
+
+import com.example.yukai.mydefinedview.MainActivity;
+import com.example.yukai.mydefinedview.R;
+
+/**
+ * Created by yukai on 2018/1/26.
+ */
+
+public class Chapter5_1 extends Activity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chapter5_1_layout);
+        findViewById(R.id.start_notification_btn).setOnClickListener(this);
+        findViewById(R.id.start_fold_notification_btn).setOnClickListener(this);
+        findViewById(R.id.start_hang_style_notification_btn).setOnClickListener(this);
+    }
+
+
+    private void createNotification(){
+        Notification notification = new NotificationCompat.Builder(this)
+                /**设置通知左边的大图标**/
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                /**设置通知右边的小图标**/
+                .setSmallIcon(R.mipmap.ic_launcher)
+                /**通知首次出现在通知栏，带上升动画效果的**/
+                .setTicker("通知来了")
+                /**设置通知的标题**/
+                .setContentTitle("这是一个通知的标题")
+                /**设置通知的内容**/
+                .setContentText("这是一个通知的内容这是一个通知的内容")
+                /**通知产生的时间，会在通知信息里显示**/
+                .setWhen(System.currentTimeMillis())
+                /**设置该通知优先级**/
+                .setPriority(Notification.PRIORITY_DEFAULT)
+                /**设置这个标志当用户单击面板就可以让通知将自动取消**/
+                .setAutoCancel(true)
+                /**设置他为一个正在进行的通知。他们通常是用来表示一个后台任务,用户积极参与(如播放音乐)或以某种方式正在等待,因此占用设备(如一个文件下载,同步操作,主动网络连接)**/
+                .setOngoing(false)
+                /**向通知添加声音、闪灯和振动效果的最简单、最一致的方式是使用当前的用户默认设置，使用defaults属性，可以组合：**/
+                .setDefaults(Notification.DEFAULT_VIBRATE | Notification.DEFAULT_SOUND)
+                .setContentIntent(PendingIntent.getActivity(this, 1, new Intent(this, MainActivity.class), PendingIntent.FLAG_CANCEL_CURRENT))
+                .build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        /**发起通知**/
+        notificationManager.notify(0, notification);
+    }
+
+    private void createFoldNotification(){
+        //先设定RemoteViews
+        RemoteViews view_custom = new RemoteViews(getPackageName(), R.layout.view_custom);
+        //设置对应IMAGEVIEW的ID的资源图片
+        view_custom.setImageViewResource(R.id.custom_icon, R.drawable.hilton);
+        view_custom.setTextViewText(R.id.tv_custom_title, "今日头条");
+        view_custom.setTextColor(R.id.tv_custom_title, Color.BLACK);
+        view_custom.setTextViewText(R.id.tv_custom_content, "金州勇士官方宣布球队已经解雇了主帅马克-杰克逊，随后宣布了最后的结果。");
+        view_custom.setTextColor(R.id.tv_custom_content, Color.BLACK);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setContent(view_custom)
+                .setContentIntent(PendingIntent.getActivity(this, 4, new Intent(this, MainActivity.class), PendingIntent.FLAG_CANCEL_CURRENT))
+                .setWhen(System.currentTimeMillis())// 通知产生的时间，会在通知信息里显示
+                .setTicker("有新资讯")
+                .setPriority(Notification.PRIORITY_HIGH)// 设置该通知优先级
+                .setOngoing(false)//不是正在进行的   true为正在进行  效果和.flag一样
+                .setSmallIcon(R.drawable.hilton);
+        Notification notify = mBuilder.build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(4, notify);
+    }
+
+    private void createHangStyleNotification(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://blog.csdn.net/itachi85/"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, mIntent, 0);
+        builder.setContentIntent(pendingIntent);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        builder.setAutoCancel(true);
+        builder.setContentTitle("悬挂式通知");
+        //设置点击跳转
+        Intent hangIntent = new Intent();
+        hangIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        hangIntent.setClass(this, MainActivity.class);
+        //如果描述的PendingIntent已经存在，则在产生新的Intent之前会先取消掉当前的
+        PendingIntent hangPendingIntent = PendingIntent.getActivity(this, 0, hangIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+        builder.setFullScreenIntent(hangPendingIntent, true);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(3, builder.build());
+    }
+
+    //low level notification deprated
+    private void createNotificationLowLevel(){
+        Notification notification = new Notification();
+        notification.icon = R.drawable.hilton;
+        notification.tickerText = "this is ticker text";
+        notification.when = System.currentTimeMillis();
+        notification.flags = Notification.FLAG_AUTO_CANCEL;
+        Intent intent = new Intent(this, NotificationActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //low level no such method
+        //notification.setLatestEventInfo
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(1, notification);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.start_notification_btn){
+            createNotification();
+        }else if (id == R.id.start_fold_notification_btn){
+            createFoldNotification();
+        }else if (id == R.id.start_hang_style_notification_btn){
+            createHangStyleNotification();
+        }
+    }
+
+}
